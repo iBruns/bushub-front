109274e923be2ef9c1f50c55fea3f41691eac4e5
Igor <igor.holanda.main@gmail.com>
2024-03-14 18:40:52 -0300

    remove theme switch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,69 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-import Button from '@/components/Button';
-import Header from '@/components/Header';
-import Switch from '@/components/Switch';
-import SearchTripForm from '@/components/SearchTripForm';
-import ShinyButton from '@/components/ShinyButton';
-import SocialButton from '@/components/SocialButton';
-import { IoAt, IoAtCircle, IoLogoGithub, IoLogoLinkedin } from 'react-icons/io5';
+import Button from "@/components/Button";
+import Header from "@/components/Header";
+import Switch from "@/components/Switch";
+import SearchTripForm from "@/components/SearchTripForm";
+import ShinyButton from "@/components/ShinyButton";
+import SocialButton from "@/components/SocialButton";
+import {
+  IoAt,
+  IoAtCircle,
+  IoLogoGithub,
+  IoLogoLinkedin,
+} from "react-icons/io5";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className='max-w-5xl mx-auto sm:px-6 lg:px-8 '>
+    <main className="max-w-5xl mx-auto sm:px-6 lg:px-8 ">
       <Header />
       <SearchTripForm />
-      <div className=' bg-gradient-to-t from-stone-900 fixed w-full h-[30vh] bottom-0 left-0' />
-      <div className='w-full mx-auto px-1 mt-[50vh]'>
-        <div className='w-full pt-12 px-6 border-t border-stone-700 flex flex-col justify-center items-center '>
-          <p className='text-stone-200 text-justify'>
-            BusHub é um projeto desenvolvido em Next.js, Tailwind e Typescript que visa criar um header com animações CSS de transição, especialmente projetadas para um site de compra de passagens de ônibus. Uma das principais características desse header é a animação do formulário, que cola no topo do header enquanto o usuário navega pelo site. Essa abordagem proporciona uma experiência visualmente atraente e fluida, tornando a interação do usuário mais intuitiva e agradável. Para conhecer mais sobre o meu trabalho, visite o meu portfólio através do botão abaixo, onde você poderá encontrar mais detalhes e exemplos de projetos semelhantes.
+      <div className=" bg-gradient-to-t from-stone-900 fixed w-full h-[30vh] bottom-0 left-0" />
+      <div className="w-full mx-auto px-1 mt-[50vh]">
+        <div className="w-full pt-12 px-6 border-t border-stone-700 flex flex-col justify-center items-center ">
+          <p className="text-stone-200 text-justify">
+            BusHub é um projeto desenvolvido em Next.js, Tailwind e Typescript
+            que visa criar um header com animações CSS de transição,
+            especialmente projetadas para um site de compra de passagens de
+            ônibus. Uma das principais características desse header é a animação
+            do formulário, que cola no topo do header enquanto o usuário navega
+            pelo site. Essa abordagem proporciona uma experiência visualmente
+            atraente e fluida, tornando a interação do usuário mais intuitiva e
+            agradável. Para conhecer mais sobre o meu trabalho, visite o meu
+            portfólio através do botão abaixo, onde você poderá encontrar mais
+            detalhes e exemplos de projetos semelhantes.
           </p>
-          <div className='flex items-center justify-center h-20 w-full mt-10'>
-
+          <Link
+            href="https://igordamm.com/"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="flex items-center justify-center h-20 w-full mt-10"
+          >
             <ShinyButton />
-          </div>
-          <div className='relative z-10 flex items-center justify-center  w-full border-b  border-stone-700 space-x-6 pb-10 pt-6 mb-[100vh]'>
-            <SocialButton leftIcon={<IoLogoGithub className='text-lg' />}>/igordamm</SocialButton>
-            <SocialButton leftIcon={<IoAtCircle className='text-lg' />}>igor.gomes.dev@gmail.com</SocialButton>
-            <SocialButton leftIcon={<IoLogoLinkedin className='text-lg' />}>/igor-gomes-pereira</SocialButton>
+          </Link>
+          <div className="relative z-10 flex items-center justify-center  w-full border-b  border-stone-700 space-x-6 pb-10 pt-6 mb-[100vh]">
+            <SocialButton
+              link="https://github.com/gmsIgor"
+              leftIcon={<IoLogoGithub className="text-lg" />}
+            >
+              /gmsIgor
+            </SocialButton>
+            <SocialButton
+              link={`mailto:igor.gomes.dev@gmail.com?subject=Contato Através do Portfólio&body=Olá, Igor! Me chamo (seu nome aqui), sou da empresa (o nome da empresa aqui),%0D%0AGostaria de falar a respeito de...`}
+              leftIcon={<IoAtCircle className="text-lg" />}
+            >
+              igor.gomes.dev@gmail.com
+            </SocialButton>
+            <SocialButton
+              link="https://www.linkedin.com/in/igor-gomes-pereira/"
+              leftIcon={<IoLogoLinkedin className="text-lg" />}
+            >
+              /igor-gomes-pereira
+            </SocialButton>
           </div>
         </div>
       </div>
-      <div className='fixed flex items-center justify-center top-[1.85rem] right-[1.85rem] '>
-        <Switch />
-      </div>
     </main>
-  )
+  );
 }
--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -1,28 +1,39 @@
-import { CSSProperties } from "react"
+import Link from "next/link";
+import { CSSProperties } from "react";
 
 interface ButtonProps {
-    children: React.ReactNode
-    className?: string
-    contentClassName?: string
-    leftIcon?: React.ReactNode
-    style?: CSSProperties
+  children: React.ReactNode;
+  className?: string;
+  contentClassName?: string;
+  leftIcon?: React.ReactNode;
+  style?: CSSProperties;
+  link: string;
 }
 
+export default function SocialButton({
+  children,
+  contentClassName,
+  leftIcon,
+  style,
+  link,
+}: ButtonProps) {
+  const focus = "focus:outline-none focus:ring";
+  const font = "text-sm text-center";
 
-export default function SocialButton({ children, contentClassName, leftIcon, style }: ButtonProps) {
+  const styleSettings = `${focus} ${font}`;
 
-    const focus = 'focus:outline-none focus:ring'
-    const font = 'text-sm text-center'
-
-    const styleSettings = `${focus} ${font}`
-
-    return (
-        <button className={`bg-[#1c19177d] hover:bg-stone-800 border border-stone-700 text-stone-400 h-10 px-5 rounded-lg transition-all duration-200 font-bold align-top ${styleSettings}`}
-            style={style}
+  return (
+    <Link href={link} rel="noopener noreferrer" target="_blank">
+      <button
+        className={`bg-[#1c19177d] hover:bg-stone-800 border border-stone-700 text-stone-400 h-10 px-5 rounded-lg transition-all duration-200 font-bold align-top ${styleSettings}`}
+        style={style}
+      >
+        <div
+          className={`h-full w-full flex flex-row space-x-3 items-center justify-center ${contentClassName}`}
         >
-            <div className={`h-full w-full flex flex-row space-x-3 items-center justify-center ${contentClassName}`}>
-                {leftIcon} {children && <span className="truncate">{children}</span>}
-            </div>
-        </button>
-    )
+          {leftIcon} {children && <span className="truncate">{children}</span>}
+        </div>
+      </button>
+    </Link>
+  );
 }
